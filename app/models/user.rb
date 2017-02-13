d3c5536b504a714188e8e858a130e9362ee93404
Mike Nguyen <mike@toch.im>
2017-02-12 23:54:14 -0800

    Add phony_rails and ensure phone number is unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   has_many :attendances
   has_many :events, through: :attendances
+
+  # Ensure phone input looks like a phone number
+  phony_normalize :phone, default_country_code: 'US'
+  validates :phone, phony_plausible: true, uniqueness: true
 end
