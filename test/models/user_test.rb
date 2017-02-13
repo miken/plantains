5aa46ec3f0ec7f3ed68745f1c7cdca12f8960f5c
Mike Nguyen <mike@toch.im>
2017-02-13 09:52:50 -0800

    Add validation for phone number from user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,4 +5,34 @@ class UserTest < ActiveSupport::TestCase
     user = User.new
     assert_not user.save, "Saved the user without any value"
   end
+
+  test "should not save without valid phone format" do
+    user = User.new phone: "blah"
+    assert_not user.save, "Saved the user with an invalid phone format"
+  end
+
+  test "should not save without valid US phone number" do
+    user = User.new phone: "844 123456789"
+    assert_not user.save, "Saved the user with an invalid US phone number"
+  end
+
+  test "should not save with fake US phone number" do
+    user = User.new phone: "123 456 7890"
+    assert_not user.save, "Saved the user with a fake US phone number"
+  end  
+
+  test "should save with valid US phone number 1" do
+    user = User.new phone: "818 456 7890"
+    assert user.save, "Did not save the user with valid US phone number"
+  end
+
+  test "should save with valid US phone number 2" do
+    user = User.new phone: "+1 818 456 7890"
+    assert user.save, "Did not save the user with valid US phone number"
+  end
+
+  test "should save with valid US phone number 3" do
+    user = User.new phone: "(818) 456-7890"
+    assert user.save, "Did not save the user with valid US phone number"
+  end
 end
