e337aef70a9da6c1483772f940eeee3440f43997
Mike Nguyen <mike@toch.im>
2017-02-13 09:47:23 -0800

    Add spec for presence of email or phone number

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,15 @@ class User < ApplicationRecord
   has_many :attendances
   has_many :events, through: :attendances
 
+  validate :phone_or_email_present?
+
   # Ensure phone input looks like a phone number
   phony_normalize :phone, default_country_code: 'US'
   validates :phone, phony_plausible: true, uniqueness: true
+
+  def phone_or_email_present?
+    if %w(phone email).all?{ |attr| self[attr].blank? }
+      errors.add :base, "User must have at least phone or email"
+    end
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save user without any value" do
+    user = User.new
+    assert_not user.save, "Saved the user without any value"
+  end
 end
