6da4e86b4b98e9ad260f227070545463d1e03304
Mike Nguyen <mike@toch.im>
2017-02-13 18:36:59 -0800

    Streamline attendance scaffold

--- app/views/attendances/_attendance.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! attendance, :id, :created_at, :updated_at
-json.url attendance_url(attendance, format: :json)
--- db/migrate/20170213072325_change_phone_type_in_users.rb
+++
@@ -1,9 +0,0 @@
-class ChangePhoneTypeInUsers < ActiveRecord::Migration[5.0]
-  def self.up
-    change_column :users, :phone, :string
-  end
- 
-  def self.down
-    change_column :users, :phone, 'integer USING CAST(phone AS integer)'
-  end
-end
