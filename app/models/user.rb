ad409e3f29aa65facc5b1b4c93b350528b1072f4
Mike Nguyen <mike@toch.im>
2017-02-13 19:05:29 -0800

    Show event attendance on user page

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,6 @@ class User < ApplicationRecord
   validate :email_blank_or_correct_format?
   validates_uniqueness_of :phone, :email
 
-
   def phone_or_email_present?
     if %w(phone email).all?{ |attr| self[attr].blank? }
       errors.add :base, "User must have at least phone or email"
@@ -20,5 +19,9 @@ class User < ApplicationRecord
     unless email.blank? or email =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
       errors.add :email, "Must be a valid email format"
     end
+  end
+
+  def total_points
+    attendances.pluck(:points_awarded).sum
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :events
   get 'welcome/index'
   root 'welcome#index'
+  resources :users, only: [:show]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
