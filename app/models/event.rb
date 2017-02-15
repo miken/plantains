d7dff822dd03cee2a949d28e7b972b08aaa38966
Mike Nguyen <mike@toch.im>
2017-02-15 12:42:22 -0800

    Validate unique code

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
   has_many :attendances
   has_many :users, through: :attendances
+  validates :name, presence: true
+  validates :code, uniqueness: true
 end
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,4 +1,14 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
+  test "should not save event without any value" do
+    event = Event.new
+    assert_not event.save, "Saved the event without any value"
+  end
+
+  test "should not save event with duped code" do
+    first = Event.create name: "First", code: "FIRST"
+    first_again = Event.new name: first.name, code: first.code
+    assert_not first_again.save, "Saved the event with a duped event code"
+  end
 end
